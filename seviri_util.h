f292557fc90c5b103213c247e7aaa8473ce2b54c
Greg McGarragh <gregory.mcgarragh@physics.ox.ac.uk>
2017-12-03 16:18:39 -0500

    Update Copyright dates.

--- seviri_util.h
+++ seviri_util.h
@@ -1,6 +1,6 @@
 /******************************************************************************%
  *
- *    Copyright (C) 2014-2015 Greg McGarragh <mcgarragh@atm.ox.ac.uk>
+ *    Copyright (C) 2014-2017 Greg McGarragh <mcgarragh@atm.ox.ac.uk>
  *
  *    This source code is licensed under the GNU General Public License (GPL),
  *    Version 3.  See the file COPYING for more details.
