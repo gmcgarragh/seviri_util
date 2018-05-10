07f22508fbcdb0d9ea1fa389fc0178d17474ea1d
Greg McGarragh <g.mcgarragh@physics.ox.ac.uk>
2018-05-10 11:12:52 -0600

    Update Copyright dates.

--- hrit_anc_funcs.h
+++ hrit_anc_funcs.h
@@ -1,6 +1,6 @@
 /******************************************************************************%
  *
- *    Copyright (C) 2015-2017 Simon Proud (simon.proud@physics.ox.ac.uk)
+ *    Copyright (C) 2015-2018 Simon Proud (simon.proud@physics.ox.ac.uk)
  *
  *    This source code is licensed under the GNU General Public License (GPL),
  *    Version 3.  See the file COPYING for more details.
