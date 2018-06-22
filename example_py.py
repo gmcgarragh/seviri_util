f5939a27f1169f8865d10d88d899252beeb0345f
Greg McGarragh <g.mcgarragh@physics.ox.ac.uk>
2018-06-21 22:28:00 -0600

    Python print as a function in the Python example.

--- example_py.py
+++ example_py.py
@@ -17,23 +17,23 @@ try:
         'BT', 'BT'], 'line_column', pixel_coords = (1899, 2199, 1700, 2299),
         do_gsics = False)
 except seviri_preproc.error:
-    'ERROR: seviri_preproc.init()'
+    print('ERROR: seviri_preproc.init()')
     exit()
 
 # Print the values for the central pixel.
 i_line   = util.n_lines / 2
 i_column = util.n_columns / 2
 
-print 'i_line:                       %d'    % i_line
-print 'i_column:                     %d'    % i_column
-print 'Julian Day Number:            % .8e' % util.time[i_line, i_column]
-print 'latitude:                     % .8e' % util.lat[i_line, i_column]
-print 'longitude:                    % .8e' % util.lon [i_line, i_column]
-print 'solar zenith angle:           % .8e' % util.sza [i_line, i_column]
-print 'solar azimuth angle:          % .8e' % util.saa [i_line, i_column]
-print 'viewing zenith angle:         % .8e' % util.vza [i_line, i_column]
-print 'viewing azimuth angle:        % .8e' % util.vaa [i_line, i_column]
-print '0.635 reflectance:            % .8e' % util.data[0, i_line, i_column]
-print '1.64  reflectance:            % .8e' % util.data[1, i_line, i_column]
-print '8.70  brightness temperature: % .8e' % util.data[2, i_line, i_column]
-print '10.80 brightness temperature: % .8e' % util.data[3, i_line, i_column]
+print('i_line:                       %d'    % i_line)
+print('i_column:                     %d'    % i_column)
+print('Julian Day Number:            % .8e' % util.time[i_line, i_column])
+print('latitude:                     % .8e' % util.lat[i_line, i_column])
+print('longitude:                    % .8e' % util.lon [i_line, i_column])
+print('solar zenith angle:           % .8e' % util.sza [i_line, i_column])
+print('solar azimuth angle:          % .8e' % util.saa [i_line, i_column])
+print('viewing zenith angle:         % .8e' % util.vza [i_line, i_column])
+print('viewing azimuth angle:        % .8e' % util.vaa [i_line, i_column])
+print('0.635 reflectance:            % .8e' % util.data[0, i_line, i_column])
+print('1.64  reflectance:            % .8e' % util.data[1, i_line, i_column])
+print('8.70  brightness temperature: % .8e' % util.data[2, i_line, i_column])
+print('10.80 brightness temperature: % .8e' % util.data[3, i_line, i_column])
