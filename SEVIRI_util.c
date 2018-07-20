gmcgarragh/seviri_util

commit bbb2eadc5aadba36b336c6c14f0c2676a43abe19

Add support for GSICS calibration coefficients, which are present in the header for most L1.5 data but previously unused. If GSICS calibration values are unavailable the utility will default to the IMPF calibration (i.e: Same method as before) and display a warning. NOTE: As of June 2018 there are no GSICS coefficients for the VIS channels (0.6,0.8,1.6,HRV).

SEVIRI_util.c
@@ -40,6 +40,7 @@
  *            List one of these per line to save the appropriate variable
  *            Adding a line with 'compress' to the driver will compress
  *            the output data, saves disk space at expense of read/write time
+ *            calib will apply GSICS calibration coefficients.
  *
  *
  *******************************************************************************
@@ -61,6 +62,7 @@
  *   vza
  *   vaa
  *   compress
+ *   calib
  *
  *   This example will read HRIT data from 12:00 UTC on 3rd March 2006
  *   Will save in the HDF5 file format
@@ -72,7 +74,7 @@
  *
  ******************************************************************************/
 
-#include "SEVIRI_tool.h"
+#include "SEVIRI_util.h"
 
 int main(int argc, char *argv[])
 {

SEVIRI_util.h
@@ -67,6 +67,7 @@ struct driver_data {
      /* ancsave contains: time,lat,lon,sza,saa,vza,vaa */
      int               ancsave[7];
      int               compression;
+     int               do_calib;
 };
 
 
@@ -84,6 +85,4 @@ int run_sev_hrit(struct driver_data driver,struct seviri_preproc_data *preproc);
 int save_sev_tiff(struct driver_data driver,struct seviri_preproc_data preproc);
 int save_sev_cdf(struct driver_data driver,struct seviri_preproc_data preproc);
 int save_sev_hdf(struct driver_data driver,struct seviri_preproc_data preproc);
-
-
 

SEVIRI_util_funcs.c
@@ -11,7 +11,7 @@
 **
 *******************************************************************************/
 
-#include "SEVIRI_tool.h"
+#include "SEVIRI_util.h"
 
 /* Sets up the band names. good for telling which band is which, easier than
    channel number */
@@ -124,6 +124,8 @@ int print_driver(struct driver_data driver)
      if (driver.compression!=1 && driver.outfrmt==SEVIRI_OUTFILE_CDF)printf("The output file will not be compressed\n");
      if (driver.compression==1 && driver.outfrmt==SEVIRI_OUTFILE_HDF)printf("The output file will be compressed with shuffle and deflate level 2\n");
      if (driver.compression!=1 && driver.outfrmt==SEVIRI_OUTFILE_HDF)printf("The output file will not be compressed\n");
+     if (driver.do_calib==1)printf("The GSICS calibration coefficients will be applied.\n");
+     if (driver.do_calib!=1)printf("The GSICS calibration coefficients will NOT be applied.\n");
 
      printf("**************************************************************************\n\n");
      return 0;
@@ -336,14 +338,18 @@ int parse_driver(char *fname,struct driver_data *driver)
      for (i=0;i<7;i++) driver->ancsave[i]=0;
      while (fgets(line, 10, fp)) {
           line[strlen(line)-1]='\0';
-          if (strcmp(line,"time")==0)     driver->ancsave[0]=1;
-          if (strcmp(line,"compress")==0) driver->compression=1;
+          if (strcmp(line,"time")==0)    driver->ancsave[0]=1;
+          if (strcmp(line,"compress")==0)driver->compression=1;
           if (strcmp(line,"lat")==0)     driver->ancsave[1]=1;
           if (strcmp(line,"lon")==0)     driver->ancsave[2]=1;
           if (strcmp(line,"sza")==0)     driver->ancsave[3]=1;
           if (strcmp(line,"saa")==0)     driver->ancsave[4]=1;
           if (strcmp(line,"vza")==0)     driver->ancsave[5]=1;
           if (strcmp(line,"vaa")==0)     driver->ancsave[6]=1;
+          if (strcmp(line,"calib")==0)
+             driver->do_calib=1;
+          else
+             driver->do_calib=0;
      }
      fclose(fp);
 

SEVIRI_util_prog.c
@@ -10,7 +10,7 @@
  *
  ******************************************************************************/
 
-#include "SEVIRI_tool.h"
+#include "SEVIRI_util.h"
 #include <tiffio.h>
 #include <netcdf.h>
 #include <hdf5.h>
@@ -28,7 +28,7 @@
 int run_sev_native(struct driver_data driver,struct seviri_preproc_data *preproc)
 {
      if (seviri_read_and_preproc(driver.infdir,preproc, driver.sev_bands.nbands, driver.sev_bands.band_ids,
-     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., 0))
+     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., driver.do_calib, 0))
      {E_L_R();}
      return 0;
 }
@@ -46,7 +46,7 @@ int run_sev_native(struct driver_data driver,struct seviri_preproc_data *preproc
 int run_sev_hrit(struct driver_data driver,struct seviri_preproc_data *preproc)
 {
      if (seviri_read_and_preproc_hrit(driver.infdir,driver.timeslot,driver.satnum, preproc, driver.sev_bands.nbands, driver.sev_bands.band_ids,
-     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., 0, 0))
+     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., 0, 0, driver.do_calib, 0))
      {E_L_R();}
      return 0;
 }

preproc.c
@@ -42,7 +42,7 @@ static double TIME_CDS_SHORT_to_jtime(const struct seviri_TIME_CDS_SHORT_data *d
  * returns	: Non-zero on error
  ******************************************************************************/
 int seviri_preproc(const struct seviri_data *d, struct seviri_preproc_data *d2,
-                   const enum seviri_units *band_units, int rss, int do_not_alloc)
+                   const enum seviri_units *band_units, int rss, int do_gsics, int do_not_alloc)
 {
      uint i;
      uint ii;
@@ -89,6 +89,8 @@ int seviri_preproc(const struct seviri_data *d, struct seviri_preproc_data *d2,
 
      double slope;
      double offset;
+
+     float ar,br,ac,bc,gs,go;
 
      double day_of_year;
 
@@ -303,11 +305,30 @@ int seviri_preproc(const struct seviri_data *d, struct seviri_preproc_data *d2,
       *-----------------------------------------------------------------------*/
      for (i = 0; i < d->image.n_bands; ++i) {
           if (band_units[i] == SEVIRI_UNIT_RAD) {
-               slope  = d->header.RadiometricProcessing.
-                    Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Slope;
-               offset = d->header.RadiometricProcessing.
-                    Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Offset;
-
+          		ac = d->header.RadiometricProcessing.
+                        Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Offset;
+               bc = d->header.RadiometricProcessing.
+                        Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Slope;
+               slope  = bc;
+               offset  = ac;
+               if (do_gsics)
+               {
+               	gs = d->header.RadiometricProcessing.
+                       MPEFCalFeedback_data[d->image.band_ids[i] - 1].GSICSCalCoeff;
+                  go = d->header.RadiometricProcessing.
+                       MPEFCalFeedback_data[d->image.band_ids[i] - 1].GSICSOffsetCount;
+                  if (gs<0.0000001)
+                  {
+                  	fprintf(stderr,"WARNING: GSICS coefficients unavailable. Using IMPF calibration.\n");
+                  }
+                  else
+                  {
+                  	br = snu_get_br_val(bc, gs);
+                  	ar = snu_get_ar_val(ac, bc, go);
+                  	slope =  (bc / br);
+                  	offset = (ac-ar) / br;
+                  }
+               }
                for (j = 0; j < d->image.n_lines; ++j) {
                     for (k = 0; k < d->image.n_columns; ++k) {
                          i_image = j * d->image.n_columns + k;
@@ -333,10 +354,30 @@ int seviri_preproc(const struct seviri_data *d, struct seviri_preproc_data *d2,
 
      for (i = 0; i < d->image.n_bands; ++i) {
           if (band_units[i] == SEVIRI_UNIT_REF || band_units[i] == SEVIRI_UNIT_BRF) {
-               slope  = d->header.RadiometricProcessing.
-                    Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Slope;
-               offset = d->header.RadiometricProcessing.
-                    Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Offset;
+               ac = d->header.RadiometricProcessing.
+                        Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Offset;
+               bc = d->header.RadiometricProcessing.
+                        Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Slope;
+               slope  = bc;
+               offset  = ac;
+               if (do_gsics)
+               {
+               	gs = d->header.RadiometricProcessing.
+                       MPEFCalFeedback_data[d->image.band_ids[i] - 1].GSICSCalCoeff;
+                  go = d->header.RadiometricProcessing.
+                       MPEFCalFeedback_data[d->image.band_ids[i] - 1].GSICSOffsetCount;
+                  if (gs<0.0000001)
+                  {
+                  	fprintf(stderr,"WARNING: GSICS coefficients unavailable. Using IMPF calibration.\n");
+                  }
+                  else
+                  {
+                  	br = snu_get_br_val(bc, gs);
+                  	ar = snu_get_ar_val(ac, bc, go);
+                  	slope =  (bc / br);
+                  	offset = (ac-ar) / br;
+                  }
+               }
 
                b = a / band_solar_irradiance[i_sat][d->image.band_ids[i] - 1];
 
@@ -367,10 +408,30 @@ int seviri_preproc(const struct seviri_data *d, struct seviri_preproc_data *d2,
       *-----------------------------------------------------------------------*/
      for (i = 0; i < d->image.n_bands; ++i) {
           if (band_units[i] == SEVIRI_UNIT_BT) {
-               slope  = d->header.RadiometricProcessing.
-                    Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Slope;
-               offset = d->header.RadiometricProcessing.
-                    Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Offset;
+               ac = d->header.RadiometricProcessing.
+                        Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Offset;
+               bc = d->header.RadiometricProcessing.
+                        Level1_5ImageCalibration[d->image.band_ids[i] - 1].Cal_Slope;
+               slope  = bc;
+               offset  = ac;
+               if (do_gsics)
+               {
+               	gs = d->header.RadiometricProcessing.
+                       MPEFCalFeedback_data[d->image.band_ids[i] - 1].GSICSCalCoeff;
+                  go = d->header.RadiometricProcessing.
+                       MPEFCalFeedback_data[d->image.band_ids[i] - 1].GSICSOffsetCount;
+                  if (gs<0.0000001)
+                  {
+                  	fprintf(stderr,"WARNING: GSICS coefficients unavailable. Using IMPF calibration.\n");
+                  }
+                  else
+                  {
+                  	br = snu_get_br_val(bc, gs);
+                  	ar = snu_get_ar_val(ac, bc, go);
+                  	slope =  (bc / br);
+                  	offset = (ac-ar) / br;
+                  }
+               }
 /*
                nu = 1.e4 / channel_center_wavelength[d->image.band_ids[i] - 1];
 */
@@ -434,7 +495,7 @@ int seviri_read_and_preproc_nat(const char *filename,
                                 enum seviri_bounds bounds,
                                 uint line0, uint line1, uint column0, uint column1,
                                 double lat0, double lat1, double lon0, double lon1,
-                                int do_not_alloc)
+                                int do_gsics, int do_not_alloc)
 {
      struct seviri_data seviri;
      int rss=0;
@@ -445,7 +506,7 @@ int seviri_read_and_preproc_nat(const char *filename,
           return -1;
      }
 
-     if (seviri_preproc(&seviri, preproc, band_units, rss,do_not_alloc)) {
+     if (seviri_preproc(&seviri, preproc, band_units, rss, do_gsics, do_not_alloc)) {
           fprintf(stderr, "ERROR: seviri_preproc()\n");
           return -1;
      }
@@ -490,7 +551,7 @@ int seviri_read_and_preproc_hrit(const char *indir, const char *timeslot,
                                  enum seviri_bounds bounds,
                                  uint line0, uint line1, uint column0, uint column1,
                                  double lat0, double lat1, double lon0, double lon1,
-                                 int rss, int iodc, int do_not_alloc)
+                                 int rss, int iodc, int do_gsics, int do_not_alloc)
 {
      int i, proc_hrv = 0;
 
@@ -502,7 +563,7 @@ int seviri_read_and_preproc_hrit(const char *indir, const char *timeslot,
           return -1;
      }
 
-     if (seviri_preproc(&seviri, preproc, band_units, rss, do_not_alloc)) {
+     if (seviri_preproc(&seviri, preproc, band_units, rss, do_gsics, do_not_alloc)) {
          fprintf(stderr, "ERROR: seviri_preproc()\n");
           return -1;
      }
@@ -555,7 +616,7 @@ int seviri_read_and_preproc(const char *filename,
                             enum seviri_bounds bounds,
                             uint line0, uint line1, uint column0, uint column1,
                             double lat0, double lat1, double lon0, double lon1,
-                            int do_not_alloc)
+                            int do_gsics, int do_not_alloc)
 {
      char *indir;
      int satnum;
@@ -566,7 +627,7 @@ int seviri_read_and_preproc(const char *filename,
      if (strstr(filename, ".nat") != NULL) {
           if (seviri_read_and_preproc_nat(filename, preproc, n_bands, band_ids,
                band_units, bounds, line0, line1, column0, column1, lat0, lat1,
-               lon0, lon1, do_not_alloc)) {
+               lon0, lon1, do_gsics, do_not_alloc)) {
                fprintf(stderr, "ERROR: seviri_read_and_preproc_nat()\n");
                return -1;
           }
@@ -579,7 +640,7 @@ int seviri_read_and_preproc(const char *filename,
 
           if (seviri_read_and_preproc_hrit(indir, timeslot, satnum, preproc,
                n_bands, band_ids, band_units, bounds, line0, line1, column0,
-               column1, lat0, lat1, lon0, lon1, rss, iodc, do_not_alloc)) {
+               column1, lat0, lat1, lon0, lon1, rss, iodc, do_gsics, do_not_alloc)) {
                fprintf(stderr, "ERROR: seviri_read_and_preproc_hrit()\n");
                return -1;
           }

example_c.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
      if (seviri_read_and_preproc(filename, &preproc, n_bands, band_ids,
                                  band_units, SEVIRI_BOUNDS_LINE_COLUMN,
                                  line0, line1, column0, column1,
-                                 0., 0., 0., 0., 0)) {
+                                 0., 0., 0., 0., 0, 0)) {
           fprintf(stderr, "ERROR: seviri_read_and_preproc_main()\n");
           exit(1);
      }

example_f90.f90
@@ -49,7 +49,7 @@ program example_f90
      ! of the arrguments.
      if (seviri_read_and_preproc_f90(trim(filename), preproc, n_bands, band_ids, &
                             band_units, SEVIRI_BOUNDS_LINE_COLUMN, line0, line1, &
-                            column0, column1, 0.d0, 0.d0, 0.d0, 0.d0, .false.) &
+                            column0, column1, 0.d0, 0.d0, 0.d0, 0.d0, .false., .false.) &
                             .ne. 0) then
         print *, 'ERROR: seviri_read_and_preproc_f90()'
         stop -1

misc_util.c
@@ -11,6 +11,25 @@
 #include "internal.h"
 #include "misc_util.h"
 
+/*******************************************************************************
+ * Calculate and return the GSICS calibration offset from the values contained
+ * in the Level 1.5 header file
+ ******************************************************************************/
+float snu_get_ar_val(float ac, float bc, float g0)
+{
+	return (ac - (bc * g0));
+}
+
+
+/*******************************************************************************
+ * Calculate and return the GSICS calibration slope from the values contained
+ * in the Level 1.5 header file
+ ******************************************************************************/
+float snu_get_br_val(float bc, float gs)
+{
+	return (bc / gs);
+}
+
 
 /*******************************************************************************
  * Return non-zero if the current machine is Little-endian and zero if it is

commit 7599936729de5d3fc468049ef31db7e9ecad606b

Fix a major bug introduced in commit bbb2ead that meant VIS channels were never processed if GSICS coefficients were unavailable.

SEVIRI_util.c
@@ -18,16 +18,18 @@
  *   Line 2,  Input directory (if HRIT) or input file (if NAT)
  *   Line 3,  Timeslot (if HRIT, YYYYMMDDHHMM) or blank (if NAT)
  *   Line 4,  Satellite number (if HRIT, 1/2/3/4) or blank (if nat)
- *   Line 5,  Bands to read (HRIT and NAT) in format: 11010100100
+ *   Line 5,  Rapid scan coverage, 1 for yes 0 for no.
+ *   Line 6,  Indian Ocean coverage, 1 for yes 0 for no.
+ *   Line 7,  Bands to read (HRIT and NAT) in format: 11010100100
  *            1 = read this band, 0 = do not read this band
  *            max length: 11 chars. If shorter, defaults to 0 for missing bands
- *   Line 6,  Output data format: HDF, CDF or TIF
- *   Line 7,  Output data type: CNT, RAD or RBT (for count, radiance, refl/bt)
- *   Line 8,  Output directory
- *   Line 9,  Initial line
- *   Line 10, Final line
- *   Line 11, Initial column
- *   Line 12, Final column
+ *   Line 8,  Output data format: HDF, CDF or TIF
+ *   Line 9,  Output data type: CNT, RAD or RBT (for count, radiance, refl/bt)
+ *   Line 10,  Output directory
+ *   Line 11,  Initial line
+ *   Line 12, Final line
+ *   Line 13, Initial column
+ *   Line 14, Final column
  *            If final vals < initial vals: Program will quit
  *            If initial vals are < 0:      Program assumes initial vals = 0
  *            If final vals are > 3711:     Program assumes final vals = 3711
@@ -47,7 +49,9 @@
  *   Example file:
  *   HRT
  *   $HOME/my_data/RAW_SEVIRI_data/
- *   200603031200
+ *   201703031200
+ *   1
+ *   0
  *   1
  *   11100011011
  *   HDF
@@ -64,7 +68,8 @@
  *   compress
  *   calib
  *
- *   This example will read HRIT data from 12:00 UTC on 3rd March 2006
+ *   This example will read HRIT data from 12:00 UTC on 3rd March 2017
+ *   will search for IODC data but not RSS data
  *   Will save in the HDF5 file format
  *   Will convert into reflectance or brightness temp (depending on channel)
  *   Will work on a 500x1000 segment of the image

SEVIRI_util.h
@@ -38,6 +38,8 @@ struct driver_data {
      char              *infdir;
      char              *timeslot;
      int               satnum;
+     int               rss;
+     int               iodc;
      char              read_bands[12];
      struct            bands_st sev_bands;
      int               outfrmt;

SEVIRI_util_funcs.c
@@ -256,6 +256,22 @@ int parse_driver(char *fname,struct driver_data *driver)
           else {printf("Failure reading input satellite number line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
      }
 
+     /* Read the rss mode */
+     if (getline(&line,&len,fp)==-1) {printf("Failure reading RSS flag line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
+     if (strlen(line)<1 && driver->infrmt==SEVIRI_INFILE_HRIT) {printf("Failure reading RSS flag line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
+     line[strlen(line)-1]='\0';
+     if (atoi(line)==0) driver->rss     =     0;
+     else if (atoi(line)==1) driver->rss     =     1;
+     else {printf("Failure reading RSS flag line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
+
+     /* Read the iodc mode */
+     if (getline(&line,&len,fp)==-1) {printf("Failure reading IODC flag line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
+     if (strlen(line)<1 && driver->infrmt==SEVIRI_INFILE_HRIT) {printf("Failure reading IODC flag line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
+     line[strlen(line)-1]='\0';
+     if (atoi(line)==0) driver->iodc     =     0;
+     else if (atoi(line)==1) driver->iodc     =     1;
+     else {printf("Failure reading IODC flag line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
+
      /* Read the bands to process */
      if (getline(&line,&len,fp)==-1) {printf("Failure reading bands to process line of driver file %s\n",fname);free(line);fclose(fp);E_L_R();}
      line[strlen(line)-1]='\0';

SEVIRI_util_prog.c
@@ -28,7 +28,7 @@
 int run_sev_native(struct driver_data driver,struct seviri_preproc_data *preproc, char satposstr[128])
 {
      if (seviri_read_and_preproc(driver.infdir,preproc, driver.sev_bands.nbands, driver.sev_bands.band_ids,
-     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., driver.do_calib, satposstr, 0))
+     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., driver.rss, driver.iodc, satposstr, 0))
      {E_L_R();}
      return 0;
 }
@@ -45,10 +45,8 @@ int run_sev_native(struct driver_data driver,struct seviri_preproc_data *preproc
 *******************************************************************************/
 int run_sev_hrit(struct driver_data driver,struct seviri_preproc_data *preproc, char satposstr[128])
 {
-     int iodc =0;
-     if (driver.satnum==SAT_MSG1+1) iodc=1;
      if (seviri_read_and_preproc_hrit(driver.infdir,driver.timeslot,driver.satnum, preproc, driver.sev_bands.nbands, driver.sev_bands.band_ids,
-     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., 0, iodc, driver.do_calib, satposstr, 0))
+     driver.outtype, driver.bounds,driver.iline, driver.fline, driver.icol, driver.fcol,0., 0., 0., 0., driver.rss, driver.iodc, driver.do_calib, satposstr, 0))
      {E_L_R();}
      return 0;
 }

preproc.c
@@ -53,9 +53,13 @@ static void get_cal_slope_and_offset(const struct seviri_data *d, int band_id,
                MPEFCalFeedback_data[band_id - 1].GSICSOffsetCount;
 
           if (gs < 0.0000001)
+          {
                fprintf(stderr,"WARNING: GSICS coefficients unavailable. "
                               "Using IMPF calibration.\n");
-          else {
+               *slope = bc;
+               *offset = ac;
+          }
+          else{
                br = su_get_br_val(bc, gs);
                ar = su_get_ar_val(ac, bc, go);
                *slope  = (bc / br);